74cad0f6e94b1084c142453c688b51de297580a4
Mateus <mateusoliveira469@gmail.com>
2022-03-30 11:34:48 -0400

    created-main-routes

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Login from "./Login";
+import Register from "./Register";
+import Menu from "./Menu";
+import Habits from "./Habits";
+import Today from "./Today";
+import History from "./History";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App () {
+    return (
+        
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/habits" element={<Habits />} />
+                <Route path="/success" element={<Today />} />
+                <Route path="/history" element={<History />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+
+    );
+}
